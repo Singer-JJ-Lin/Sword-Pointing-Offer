0af94e82fe35d3ffe4d030c5f1239d9bef9a67ef
Jason_A_Fan_of_JJ_Lin <764901951@qq.com>
2023-01-10 11:44:21 +0800

    2023/01/10)

--- 0018-删除链表的节点/problem18.go
+++ 0018-删除链表的节点/problem18.go
@@ -1,1 +1,33 @@
 package _18_删除链表的节点
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteNode(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	if head.Val == val {
+		return head.Next
+	}
+
+	dummy := ListNode{Val: 1<<31 - 1, Next: head}
+	pre, ptr := head, head.Next
+
+	for ptr != nil {
+		if ptr.Val == val {
+			temp := ptr
+			pre.Next = ptr.Next
+			ptr = temp.Next
+		} else {
+
+			pre = ptr
+			ptr = ptr.Next
+		}
+	}
+
+	return dummy.Next
+}
--- 0022-链表中倒数第K个节点/problem22.go
+++ 0022-链表中倒数第K个节点/problem22.go
@@ -1,1 +1,21 @@
-package _20_链表中倒数第K个节点
+package _22_链表中倒数第K个节点
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getKthFromEnd(head *ListNode, k int) *ListNode {
+	fast, slow := head, head
+	for k > 0 && fast != nil {
+		fast = fast.Next
+		k = k - 1
+	}
+
+	for fast != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+
+	return slow
+}
