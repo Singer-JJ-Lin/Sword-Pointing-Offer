5572d6697cc3a71c25c86a2b5f6414bff6e5ef55
Jason_A_Fan_of_JJ_Lin <764901951@qq.com>
2023-01-10 11:48:29 +0800

    2023/01/10

--- 0009-用两个栈实现队列/problem09.go
+++ 0009-用两个栈实现队列/problem09.go
@@ -1,1 +1,28 @@
 package _009_用两个栈实现队列
+
+type CQueue struct {
+	inStack, outStack []int
+}
+
+func Constructor() CQueue {
+	return CQueue{}
+}
+
+func (this *CQueue) AppendTail(value int) {
+	this.inStack = append(this.inStack, value)
+}
+
+func (this *CQueue) DeleteHead() int {
+	if len(this.outStack) == 0 {
+		if len(this.inStack) == 0 {
+			return -1
+		}
+		for len(this.inStack) != 0 {
+			this.outStack = append(this.outStack, this.inStack[len(this.inStack)-1])
+			this.inStack = this.inStack[:len(this.inStack)-1]
+		}
+	}
+	val := this.outStack[len(this.outStack)-1]
+	this.outStack = this.outStack[:len(this.outStack)-1]
+	return val
+}
