7559765b7f2f3fd06047731a57ecfbd537f5ba61
Jason_A_Fan_of_JJ_Lin <764901951@qq.com>
2023-01-11 09:08:22 +0800

    2023/01/11

--- 0052-两个链表的第一个公共节点/problem52.go
+++ 0052-两个链表的第一个公共节点/problem52.go
@@ -1,1 +1,75 @@
 package _52_两个链表的第一个公共节点
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 比较长度的方法
+func getIntersectionNode1(headA, headB *ListNode) *ListNode {
+	len_A, len_B := 0, 0
+	ptr_A, ptr_B := headA, headB
+
+	//统计链表A和链表B的长度
+	for ptr_A != nil {
+		len_A++
+		ptr_A = ptr_A.Next
+	}
+
+	for ptr_B != nil {
+		len_B++
+		ptr_B = ptr_B.Next
+	}
+
+	// 重新设置节点
+	ptr_A, ptr_B = headA, headB
+
+	//如果链表A长，那么链表A先走
+	if len_A > len_B {
+		difference := len_A - len_B
+		for difference > 0 {
+			ptr_A = ptr_A.Next
+			difference = difference - 1
+		}
+		//如果链表B长，那么链表B先走
+	} else if len_B > len_A {
+		difference := len_B - len_A
+		for difference > 0 {
+			ptr_B = ptr_B.Next
+			difference = difference - 1
+		}
+	}
+
+	//然后开始比较，如果他俩不相等就一直往下走
+	for ptr_A != nil && ptr_B != nil && ptr_A != ptr_B {
+		ptr_A = ptr_A.Next
+		ptr_B = ptr_B.Next
+	}
+
+	// 跳出循环后无非两种结果
+	// 1.相交
+	// 2.没有相交的节点，此时ptr_A和ptr_B均为nil
+	return ptr_A
+
+}
+
+// 双指针的方法
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	// 设置指针
+	ptr_A, ptr_B := headA, headB
+	for ptr_A != ptr_B {
+		if ptr_A != nil {
+			ptr_A = ptr_A.Next
+		} else {
+			ptr_A = headB
+		}
+
+		if ptr_B != nil {
+			ptr_B = ptr_B.Next
+		} else {
+			ptr_B = headA
+		}
+	}
+
+	return ptr_A
+}
--- 0052-两个链表的第一个公共节点/problem52.md
+++ 0052-两个链表的第一个公共节点/problem52.md
@@ -1,1 +1,11 @@
 # 两个链表的第一个公共节点
+## 比较链表长度的方法
+1. 统计两个链表A，B的长度len_A, len_B   
+2. 如果len_A > len_B, 则A先走`len_A - len_B`步  
+如果len_B > len_A, 则B先走`len_B - len_A`步
+3. 然后逐个节点比较
+
+## 双指针的方法
+
+我们设置两个指针ptr_A指向headA, ptr_B指向headB, 然后ptr_A和ptr_B同时走。当ptr_A到达链表尾时，ptr_A指向headB，当ptr_B到达链表尾时，ptr_B指向headA。
+当两个指针都经历过重新指向headA和headB的过程后，两个指针距离公共节点的距离是相同的，只需不断的往后走到第一个相同的节点即可
