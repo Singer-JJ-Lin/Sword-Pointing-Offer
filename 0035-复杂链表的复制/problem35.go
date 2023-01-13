a7fa5592edb608e9aba3cb3cd2ef51273b851412
Jason_A_Fan_of_JJ_Lin <764901951@qq.com>
2023-01-13 11:28:42 +0800

    2023/01/13

--- 0035-复杂链表的复制/problem35.go
+++ 0035-复杂链表的复制/problem35.go
@@ -7,14 +7,19 @@ type Node struct {
 }
 
 func copyRandomList(head *Node) *Node {
+	// 特殊判断
 	if head == nil {
 		return nil
 	}
 
+	// 建立哑节点
 	dummy := new(Node)
+
+	// 设立ptr指向旧链表，new_ptr指向新链表
 	ptr, new_ptr := head, dummy
 	mapper := make(map[*Node]*Node)
 
+	// 构建新链表同时建立映射关系
 	for ptr != nil {
 		node := new(Node)
 		node.Val = ptr.Val
@@ -23,7 +28,10 @@ func copyRandomList(head *Node) *Node {
 		ptr, new_ptr = ptr.Next, new_ptr.Next
 	}
 
+	// reset指针
 	ptr, new_ptr = head, dummy.Next
+
+	// 构建新链表的random指针
 	for ptr != nil {
 		new_ptr.Random = mapper[ptr.Random]
 		ptr, new_ptr = ptr.Next, new_ptr.Next
