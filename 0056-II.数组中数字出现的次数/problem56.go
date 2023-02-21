ec6c9d3b9b243b0849cbf1895a7f184dde2065c7
Jason_A_Fan_of_JJ_Lin <764901951@qq.com>
2023-02-21 09:16:57 +0800

    2023/02/21

---
+++ 0056-II.数组中数字出现的次数/problem56.go
@@ -0,0 +1,10 @@
+package _56_II_数组中数字出现的次数
+
+func singleNumber(nums []int) int {
+	ones, twos := 0, 0
+	for _, num := range nums {
+		ones = ones ^ num & ^twos
+		twos = twos ^ num & ^ones
+	}
+	return ones
+}
